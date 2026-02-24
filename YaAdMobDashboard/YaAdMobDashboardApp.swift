3bba6eecdd57f28e34e1deebd2a986993604b30e
Pavel Grigorev <amorfien@gmail.com>
2026-02-24 14:21:25 +0300

    Initial Commit

---
+++ YaAdMobDashboard/YaAdMobDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaAdMobDashboardApp.swift
+//  YaAdMobDashboard
+//
+//  Created by Pavel Grigorev on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct YaAdMobDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
